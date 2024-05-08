f7987f3b5afd787be40917df382d6670a6c76580
Luke Williams <williluk@os1.engr.oregonstate.edu>
2024-05-07 18:11:29 -0700

    basic server communication test 3

--- js/communication.js
+++ js/communication.js
@@ -1,7 +1,16 @@
 async function postJSON(data, filename) 
 {
     var data;
-    const myRequest = new Request("data/" + filename, {method : "POST", body: JSON.stringify(data)});
+    const myRequest = new Request("data/" + filename, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "username": "YOUR_USERNAME",
+            "password": "YOUR_PASSWORD"
+        })
+    });
     fetch(myRequest).then((response) => response.text()).then((text) => 
     {
         
